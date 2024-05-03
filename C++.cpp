d1677545c3ab65648d232ffa3c8591d2deca17bd
askjdx <168858738+askjdx@users.noreply.github.com>
2024-05-03 21:43:06 +0300

    Add files via upload

---
+++ C++.cpp
@@ -0,0 +1,10 @@
+﻿// C++.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
